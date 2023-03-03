bd17b55b32ecce4eb8b5f444f185e6f8897c71e7
absss03 <barbieriabigailok@gmail.com>
2023-03-03 20:52:19 -0300

    creo componentes y otras cosas

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { PersonalComponent } from './componentes/personal/personal.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { FormacionComponent } from './componentes/formacion/formacion.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BannerComponent,
+    PersonalComponent,
+    ExperienciaComponent,
+    FormacionComponent,
+    SkillsComponent,
+    ProyectosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
