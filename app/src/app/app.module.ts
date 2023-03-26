d3d49f31bcc2b68be14692cc31fee9a4290f9117
absss03 <barbieriabigailok@gmail.com>
2023-03-26 17:34:00 -0300

    comento codigo

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -16,7 +16,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './componentes/login/login.component';
 import { ModalBannerComponent } from './componentes/modales/modal-banner/modal-banner.component';
 import { ModalExperienciaComponent } from './componentes/modales/modal-experiencia/modal-experiencia.component';
-import { ModalPersonalComponent } from './componentes/modales/modal-personal/modal-personal.component';
+//import { ModalPersonalComponent } from './componentes/modales/modal-personal/modal-personal.component';
 
 @NgModule({
   declarations: [
